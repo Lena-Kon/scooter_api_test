d82e24ac896ed0c94ce105dbd019ff9247e58571
Елена <ekonovalova81@yandex.ru>
2024-01-30 00:28:05 +0300

    add .configuration.ry

--- request 1.sql
+++ request 1.sql
@@ -1,7 +1,10 @@
 SELECT c.login, count(o.id)
-FROM "Couriers" c
-JOIN "Orders" o on (o."courierId" = c.id) AND o."inDelivery" = true
+FROM "Orders"
+  AS o INNER JOIN "Couriers"
+  AS c ON o."courierId" = c.id
+  WHERE o."inDelivery" = true
 GROUP BY c.login;
+
 
 SELECT c.login, count(o.id)
 FROM "Couriers" c
