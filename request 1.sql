14b7d45c6ac07856a5248db74ff17d48ce80d984
Lena-Kon <154056484+Lena-Kon@users.noreply.github.com>
2024-01-30 00:36:59 +0300

    Update request 1.sql

--- request 1.sql
+++ request 1.sql
@@ -1,12 +1,5 @@
-SELECT c.login, count(o.id)
-FROM "Orders"
-  AS o INNER JOIN "Couriers"
-  AS c ON o."courierId" = c.id
-  WHERE o."inDelivery" = true
-GROUP BY c.login;
-
-
-SELECT c.login, count(o.id)
-FROM "Couriers" c
-JOIN "Orders" o on (o."courierId" = c.id) AND o."inDelivery" = true
+SELECT c.login, COUNT(*)
+FROM "Couriers"
+    c INNER JOIN "Orders" o ON (o."courierId" = c.id)
+    AND o."inDelivery" = true
 GROUP BY c.login;
